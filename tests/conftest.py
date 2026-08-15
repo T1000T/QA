0edc9fb3de03c610f3dbe6eb3cdcdfee5f7a46cd
T1000T <giladt1000@gmail.com>
2026-08-15 14:02:47 +0300

    add screenshot on failed test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from selenium import webdriver
 from automation.pages.login_page import LoginPage
 
@@ -18,3 +19,22 @@ def driver():
 @pytest.fixture
 def login_page(driver):
     return LoginPage(driver)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+
+        if driver:
+            os.makedirs("screenshots", exist_ok=True)
+
+            screenshot_path = os.path.join(
+                "screenshots",
+                f"{item.name}.png"
+            )
+
+            driver.save_screenshot(screenshot_path)
