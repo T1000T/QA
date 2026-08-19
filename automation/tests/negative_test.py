83d1da48f10870a68ef5b575be9bfb5a3bb7d7b9
T1000T <giladt1000@gmail.com>
2026-08-19 22:21:50 +0300

    small code cleanup

--- automation/tests/negative_test.py
+++ automation/tests/negative_test.py
@@ -8,7 +8,6 @@ from test_data.login_data import *
 def test_username_password_out_of_boundaries(login_page, username_input_boundary, password_input_boundary):
     login_page.open()
     login_page.login(username_input_boundary,password_input_boundary)
-    login_page.click_login()
 
     message = login_page.get_message()
 
@@ -21,7 +20,6 @@ def test_username_password_out_of_boundaries(login_page, username_input_boundary
 def test_password_length(login_page, username_input_length, password_input_length):
     login_page.open()
     login_page.login(username_input_length, password_input_length)
-    login_page.click_login()
 
     message = login_page.get_message()
 
@@ -34,7 +32,42 @@ def test_password_length(login_page, username_input_length, password_input_lengt
 def test_username_password_mismatch(login_page, username_input_mismatch, password_input_mismatch):
     login_page.open()
     login_page.login(username_input_mismatch, password_input_mismatch)
-    login_page.click_login()
+
+    message = login_page.get_message()
+
+    assert "Invalid username or password" in message
+
+# empty field
+@pytest.mark.negative
+@pytest.mark.parametrize("test_username, test_password", empty_data)
+
+def test_login_requires_username_and_password(login_page, test_username, test_password):
+    login_page.open()
+    login_page.login(test_username, test_password)
+
+    message = login_page.get_message()
+
+    assert "Username and password are required" in message
+
+# locked user
+@pytest.mark.negative
+@pytest.mark.parametrize("locked_username_input, locked_password_input", locked_user)
+
+def test_locked_user(login_page, locked_username_input, locked_password_input):
+    login_page.open()
+    login_page.login(locked_username_input, locked_password_input)
+
+    message = login_page.get_message()
+
+    assert "This account is locked. Please contact support" in message
+
+# case-sensitive
+@pytest.mark.negative
+@pytest.mark.parametrize("casesensitive_username_input, casesensitive_password_input", casesensitive)
+
+def test_username_and_password_are_case_sensitive(login_page, casesensitive_username_input, casesensitive_password_input):
+    login_page.open()
+    login_page.login(casesensitive_username_input, casesensitive_password_input)
 
     message = login_page.get_message()
 
@@ -44,11 +77,9 @@ def test_username_password_mismatch(login_page, username_input_mismatch, passwor
 @pytest.mark.negative
 @pytest.mark.parametrize("username_input, password_input_whitespace", password_whitespace)
 
-@pytest.mark.negative
 def test_password_whitespace(login_page, username_input, password_input_whitespace):
     login_page.open()
     login_page.login(username_input, password_input_whitespace)
-    login_page.click_login()
 
     message = login_page.get_message()
 
--- automation/tests/positive_test.py
+++ automation/tests/positive_test.py
@@ -5,57 +5,13 @@ from test_data.login_data import *
 @pytest.mark.positive
 @pytest.mark.parametrize("username_input, password_input", successful_login)
 
-
 def test_successful_login(login_page, username_input, password_input):
     login_page.open()
     login_page.login(username_input, password_input)
-    login_page.click_login()
 
     message = login_page.get_message()
 
     assert "Login successful" in message
-
-
-# locked user
-@pytest.mark.positive
-@pytest.mark.parametrize("locked_username_input, locked_password_input", locked_user)
-
-def test_locked_user(login_page, locked_username_input, locked_password_input):
-    login_page.open()
-    login_page.login(locked_username_input, locked_password_input)
-    message = login_page.get_message()
-
-    assert "This account is locked. Please contact support" in message
-
-
-# case-sensitive
-@pytest.mark.positive
-@pytest.mark.parametrize("casesensitive_username_input, casesensitive_password_input", casesensitive)
-
-
-def test_username_and_password_are_case_sensitive(login_page, casesensitive_username_input, casesensitive_password_input):
-    login_page.open()
-    login_page.login(casesensitive_username_input, casesensitive_password_input)
-    login_page.click_login()
-
-    message = login_page.get_message()
-
-    assert "Invalid username or password" in message
-
-
-# empty field
-@pytest.mark.positive
-@pytest.mark.parametrize("test_username, test_password", empty_data)
-
-def test_login_requires_username_and_password(login_page, test_username, test_password):
-    login_page.open()
-    login_page.login(test_username, test_password)
-    login_page.click_login()
-
-    message = login_page.get_message()
-
-    assert "Username and password are required" in message
-
 
 # Verify that clicking Forgot Password displays the reset confirmation
 @pytest.mark.positive
